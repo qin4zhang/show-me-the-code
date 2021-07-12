7cbe1c51e0c2c8a0850a5344a6f6416570ce7972
zhangzhen <hbzhangzhen2010@163.com>
2021-07-12 18:41:53 +0800

    add IdWorker

---
+++ distributed-id/src/main/java/com/qin4zhang/demo/App.java
@@ -0,0 +1,15 @@
+package com.qin4zhang.demo;
+
+import java.util.UUID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
